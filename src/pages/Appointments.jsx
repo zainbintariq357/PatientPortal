4e53aed48a7cd9c19101ba7493d69862a66436fd
zainbintariq <ztariq@practiceehr.com>
2026-01-29 18:24:58 +0500

    Icon issue fixed.

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -5,7 +5,7 @@ import RequestedAppointments from '../components/Appointments/RequestedAppointme
 import SELECT_OPTIONS from '../Constants';
 import WarningMessage from '../components/popup/WarningMessage';
 import PrevIcon from '../assets/prevIcon.svg?react';
-import NextIcon from '../assets/NextIcon.svg?react';
+import NextIcon from '../assets/nextIcon.svg?react';
 
 const Appointments = () => {
 
